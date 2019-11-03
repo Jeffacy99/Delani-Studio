91fefe286a570ddb24feaee350fe61738f3969f4
Jeffacy99 <mwaniajapheth1999@gmail.com>
2019-11-03 21:59:52 +0300

    show,hide function added

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
   $(".croseover").hover(function(){
       $(this).find(".road").toggle(400);
   });
+});
+$(".click0").click(function(){
+    $(".hide0").slideToggle(1000);
+    $(".show0").slideToggle(1200);
+  });
+
+  $(".click1").click(function(){
+    $(".hide1").slideToggle(1000);
+    $(".show1").slideToggle(1200);
+  });
+
+  $(".click2").click(function(){
+    $(".hide2").slideToggle(1000);
+    $(".show2").slideToggle(1200);
+  });
